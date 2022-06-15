faabab513caf37ba57fb82fdce50281d639c4e8b
Arthur McNuke <borodoslav@gmail.com>
2022-06-15 15:32:55 +0700

    Added constructor for the 'App' component, loading of the quotes-containing file by using 'fetch' and conditional rendering for loading and error cases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React from 'react';
 import './App.css'
 
-class App extends React.Component{  
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    
+    this.state = { 
+      error: "",
+      isLoaded: false,
+      quoteIdx: 0,
+      quotesArr: null
+    }
+  }
+
+  componentDidMount(){    
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      .then((response) => response.json())
+      .then((response) => {
+          this.setState(
+            {
+              quotesArr: response.quotes, 
+              isLoaded: true,
+              quoteIdx: Math.floor(Math.random() * response.quotes.length)
+            }
+          );
+        }
+      )
+      .catch(
+        (error) => {
+          this.setState({error: "Could not load the quotes"}); 
+          console.log(error)
+        }
+      );
+  }
+
   render(){
-    let currentQuote = 'Remember no one can make you feel inferior without your consent.';
-    let currentAuthor = 'Eleanor Roosevelt';
-    return (
-      <div id = 'quote-box'>
-        <div id = 'text'>{currentQuote}</div>
-        <div id = 'author'>- {currentAuthor}</div>
-
-        <div>
-          <a id = 'tweet-quote' className='button' href = {'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)}>
-            t
-          </a>
-          <div id = 'new-quote' className='button'>Next Quote</div>
+    if (this.state.isLoaded){
+      let currentQuote = this.state.quotesArr[this.state.quoteIdx].quote;
+      let currentAuthor = this.state.quotesArr[this.state.quoteIdx].author;
+      return (
+        <div id = 'quote-box'>
+          <div id = 'text'>{currentQuote}</div>
+          <div id = 'author'>- {currentAuthor}</div>
+          <div>
+            <a id = 'tweet-quote' className='button' href = {'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)}>
+              t
+            </a>
+            <div id = 'new-quote' className='button'>Next Quote</div>
+          </div>
         </div>
-
-      </div>
-     );
-   }
+      );
+    }
+    else {
+      return (
+        <div style={{color: "white"}}>
+          {this.state.error ? this.state.error : "Loading..."}
+        </div>
+      )
+    }
+    }
+    
 }
 
 export default App;
