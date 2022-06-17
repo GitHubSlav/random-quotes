c4abafe4dc62b3b4fb6d641e4bff3dcdd0acfd2a
Arthur McNuke <borodoslav@gmail.com>
2022-06-17 15:17:52 +0700

    Connected App to redux store

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ class App extends React.Component{
   constructor(props){
     super(props);
     
-    this.state = { 
-      error: "",
-      isLoaded: false,
-      quoteIdx: 0,
-      quotesArr: null
-    }
-
+    this.quotesArr = null;
     this.wrapperRef = React.createRef();
 
     this.generateQuote = this.generateQuote.bind(this);
@@ -22,34 +16,30 @@ class App extends React.Component{
     wrapper.classList.add('fadeIn');
     setTimeout(() => wrapper.classList.remove('fadeIn'), 1000);
 
-    this.setState({quoteIdx: Math.floor(Math.random() * this.state.quotesArr.length)});
+    this.props.nextQuote(this.quotesArr.length);
   }
 
   componentDidMount(){    
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
       .then((response) => response.json())
       .then((response) => {
-          this.setState(
-            {
-              quotesArr: response.quotes, 
-              isLoaded: true,
-              quoteIdx: Math.floor(Math.random() * response.quotes.length)
-            }
-          );
+          this.quotesArr = response.quotes;
+          this.props.nextQuote(this.quotesArr.length);
+          this.props.loadApp();
         }
       )
       .catch(
         (error) => {
-          this.setState({error: "Could not load the quotes"}); 
-          console.log(error)
+          this.props.catchError();
+          console.log(error);
         }
       );
   }
 
   render(){
-    if (this.state.isLoaded){
-      let currentQuote = this.state.quotesArr[this.state.quoteIdx].quote;
-      let currentAuthor = this.state.quotesArr[this.state.quoteIdx].author;
+    if (this.props.isLoaded){
+      let currentQuote = this.quotesArr[this.props.quoteIdx].quote;
+      let currentAuthor = this.quotesArr[this.props.quoteIdx].author;
       return (
         <div id = 'quote-box' className='fadeIn'>
           <div ref = {this.wrapperRef}>
@@ -69,7 +59,7 @@ class App extends React.Component{
     else {
       return (
         <div style={{color: "white"}}>
-          {this.state.error ? this.state.error : "Loading..."}
+          {this.props.error ? this.props.error : "Loading..."}
         </div>
       )
     }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
+import { connect, Provider } from 'react-redux';
+import { mapStateToAppProps, mapDispatchToAppProps } from './mapToAppProps';
+
+const store = createStore(rootReducer);
+
+const ConnectedApp = connect(mapStateToAppProps, mapDispatchToAppProps)(App);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <ConnectedApp />
+    </Provider>
   </React.StrictMode>
 );
